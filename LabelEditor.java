efd27ebdbf0288b8e33f05e69876fca1276251b1
Igor Romanovich <82456331+IgorRomanovich@users.noreply.github.com>
2022-11-28 04:06:36 +0300

    Add files via upload

---
+++ LabelEditor.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bsu.fpmi.edupract;
+
+/**
+ *
+ * @author REDMI
+ */
+import java.beans.*;
+
+public class LabelEditor extends PropertyEditorSupport {
+    @Override
+    public String[] getTags() {
+	return new String[] { "Word", "Title", "Header" };
+    }
+    
+    @Override
+    public boolean supportsCustomEditor() { return true; }
+    
+    @Override
+    public void setAsText(String s) {
+	if (s.equals(getTags()[0])) setValue(getTags()[0]);
+	else if (s.equals(getTags()[1])) setValue(getTags()[1]);
+	else if (s.equals(getTags()[2])) setValue(getTags()[2]);
+	else throw new IllegalArgumentException(s);
+    }
+    
+    /** This is an important method for code generation. */
+    @Override
+    public String getJavaInitializationString() {
+	return "\""+ (String) getValue() + "\"";
+    }
+}

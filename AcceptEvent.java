df69e9b2cfd682ce109e5b5bbdc509b2cf9d95ad
Igor Romanovich <82456331+IgorRomanovich@users.noreply.github.com>
2022-11-28 04:07:15 +0300

    Update AcceptEvent.java

--- AcceptEvent.java
+++ AcceptEvent.java
@@ -1,9 +1,4 @@
-package bsu.fpmi.edupract;/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+package bsu.fpmi.edupract;
 
 public class AcceptEvent extends java.util.EventObject {
     public static final int MESSAGE = 0;
